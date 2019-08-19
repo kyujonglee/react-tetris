931c7e04c7bce926fed9b035936b9b63217d9485
이규종 <kyujong93@naver.com>
2019-08-20 02:17:30 +0900

    Updates

---
+++ precache-manifest.e6f8fe81ee5065fa376b59eab94060ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2005c9120adce07a45c8572634db4336",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "0f799ac5f910035bb6c9",
+    "url": "/react-tetris/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "87ea915eefc4c53953b4",
+    "url": "/react-tetris/static/js/2.b8bc6fe9.chunk.js"
+  },
+  {
+    "revision": "0f799ac5f910035bb6c9",
+    "url": "/react-tetris/static/js/main.6dac6b4a.chunk.js"
+  },
+  {
+    "revision": "3cbf365fbc4eeca393c0",
+    "url": "/react-tetris/static/js/runtime~main.1a1357f3.js"
+  }
+]);
